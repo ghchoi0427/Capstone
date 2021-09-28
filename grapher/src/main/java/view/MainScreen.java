166d2373444f9d86dd69d703e08501d8f3316679
ghchoi0427 <ghchoi0427@gmail.com>
2021-09-28 23:29:43 +0900

    refactor: renamed from formGraph

---
+++ grapher/src/main/java/view/MainScreen.java
@@ -0,0 +1,31 @@
+package view;
+
+import controller.GraphController;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainScreen extends JFrame {
+
+    public MainScreen() {
+        super("graph");
+        setBounds(100, 100, 400, 300);
+
+        JPanel panel = new JPanel();
+        Container container = this.getContentPane();
+        JButton btnConnect = new JButton("connect");
+        JTextField textAddress = new JTextField(15);
+
+        btnConnect.setBounds(0, 0, 50, 50);
+        btnConnect.setMnemonic('S');
+        btnConnect.addActionListener(e -> {
+
+        });
+
+        panel.add(textAddress);
+        panel.add(btnConnect);
+        container.add(panel);
+
+        setVisible(true);
+    }
+}
