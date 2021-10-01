635db1a78056c4bc66cea68b865257440dbbc567
ghchoi0427 <ghchoi0427@gmail.com>
2021-10-01 13:12:05 +0900

    fix: view 제외

--- grapher/src/main/java/web/BootWebApplication.java
+++ grapher/src/main/java/web/BootWebApplication.java
@@ -2,13 +2,11 @@ package web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import web.view.MainScreen;
 
 @SpringBootApplication
 public class BootWebApplication {
 
     public static void main(String[] args) {
-        new Thread(() -> new MainScreen()).run();
         SpringApplication.run(BootWebApplication.class, args);
     }
 }
