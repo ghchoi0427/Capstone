ff0fb7d8b6c34931ab74ea475723f3fd0153860b
ghchoi0427 <ghchoi0427@gmail.com>
2021-10-01 16:43:54 +0900

    feat: 정해진 시각에 기상 데이터 반환

--- grapher/src/main/java/web/controller/HomeController.java
+++ grapher/src/main/java/web/controller/HomeController.java
@@ -1,6 +1,7 @@
 package web.controller;
 
 import org.jfree.data.time.Second;
+import org.json.simple.parser.ParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,14 +10,49 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
     @GetMapping
-    public ResponseEntity<String> getData(@RequestParam(value = "temp") int temp, @RequestParam(value = "humidity") int humidity) throws IOException {
+    public ResponseEntity<String> getData(@RequestParam(value = "temp") int temp, @RequestParam(value = "humidity") int humidity) throws IOException, ParseException {
+        int returnTemp = -1;
+        int returnHumid = -1;
+
         CSVFactory.addData(new String[]{new Second().toString(), String.valueOf(temp), String.valueOf(humidity)});
-        return new ResponseEntity<>(temp + " / " + humidity, HttpStatus.OK);
+        List<Integer> returnList = getDataAtTime(returnTemp, returnHumid);
+        returnTemp = returnList.get(0);
+        returnHumid = returnList.get(1);
+        return new ResponseEntity<>(returnTemp + " / " + returnHumid, HttpStatus.OK);
     }
+
+    private List<Integer> getDataAtTime(int returnTemp, int returnHumid) throws ParseException {
+        LocalTime now = LocalTime.now();
+        System.out.println(now.getHour());
+        if (now.getHour() == 6 || now.getHour() == 11 || now.getHour() == 16) {
+            String requestHour = String.format("%02d", (now.getHour() - 1));
+            String json = APIHandler.sendRequest(getDate(), requestHour+"00");
+            returnTemp = (int) Float.parseFloat(JSONHandler.getTemperature(json));
+            returnHumid = Integer.parseInt(JSONHandler.getHumidity(json));
+            System.out.println("it's time"+returnTemp+","+returnHumid);
+        }
+        List<Integer> result = new ArrayList<>();
+        result.add(returnTemp);
+        result.add(returnHumid);
+
+        return result;
+    }
+
+    private String getDate() {
+        LocalDate now = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        return now.format(formatter);
+    }
+
 }
