858006c27f7e4e4ed976d77be4f68b6b1913df6a
ghchoi0427 <ghchoi0427@gmail.com>
2021-10-03 11:29:11 +0900

    fix: request 받으면 인수값 시스템 출력

--- grapher/src/main/java/web/controller/HomeController.java
+++ grapher/src/main/java/web/controller/HomeController.java
@@ -1,7 +1,6 @@
 package web.controller;
 
 import org.jfree.data.time.Second;
-import org.json.simple.parser.ParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -21,7 +20,7 @@ import java.util.List;
 public class HomeController {
 
     @GetMapping
-    public ResponseEntity<String> getData(@RequestParam(value = "temp") int temp, @RequestParam(value = "humidity") int humidity) throws IOException, ParseException {
+    public ResponseEntity<String> getData(@RequestParam(value = "temp") int temp, @RequestParam(value = "humidity") int humidity) throws IOException {
         int returnTemp = -1;
         int returnHumid = -1;
 
@@ -29,18 +28,21 @@ public class HomeController {
         List<Integer> returnList = getDataAtTime(returnTemp, returnHumid);
         returnTemp = returnList.get(0);
         returnHumid = returnList.get(1);
+        System.out.println("[GET]: temperature: "+temp+","+"humidity: "+humidity);
         return new ResponseEntity<>(returnTemp + " / " + returnHumid, HttpStatus.OK);
     }
 
-    private List<Integer> getDataAtTime(int returnTemp, int returnHumid) throws ParseException {
+    private List<Integer> getDataAtTime(int returnTemp, int returnHumid) {
         LocalTime now = LocalTime.now();
-        System.out.println(now.getHour());
         if (now.getHour() == 6 || now.getHour() == 11 || now.getHour() == 16) {
             String requestHour = String.format("%02d", (now.getHour() - 1));
             String json = APIHandler.sendRequest(getDate(), requestHour+"00");
-            returnTemp = (int) Float.parseFloat(JSONHandler.getTemperature(json));
-            returnHumid = Integer.parseInt(JSONHandler.getHumidity(json));
-            System.out.println("it's time"+returnTemp+","+returnHumid);
+            try {
+                returnTemp = (int) Float.parseFloat(JSONHandler.getTemperature(json));
+                returnHumid = Integer.parseInt(JSONHandler.getHumidity(json));
+            } catch (org.json.simple.parser.ParseException e) {
+                System.out.println(e.getMessage());
+            }
         }
         List<Integer> result = new ArrayList<>();
         result.add(returnTemp);
