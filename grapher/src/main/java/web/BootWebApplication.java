102290fd35cd2dfaa758ba50ebb34b0c892f5139
ghchoi0427 <ghchoi0427@gmail.com>
2021-10-03 11:30:49 +0900

    fix: MainScreen 실행

--- grapher/src/main/java/web/BootWebApplication.java
+++ grapher/src/main/java/web/BootWebApplication.java
@@ -2,11 +2,16 @@ package web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import web.view.MainScreen;
 
 @SpringBootApplication
 public class BootWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BootWebApplication.class, args);
+        if (args.length == 0) {
+            new MainScreen();
+        } else {
+            SpringApplication.run(BootWebApplication.class, args);
+        }
     }
 }
