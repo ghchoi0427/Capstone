c9709f9a69625342b7d5aa87c1d7ab24e45050e1
ghchoi0427 <ghchoi0427@gmail.com>
2021-10-19 15:20:05 +0900

    feat: Modulator 클래스 생성

---
+++ grapher/src/main/java/web/controller/Modulator.java
@@ -0,0 +1,31 @@
+package web.controller;
+
+public class Modulator {
+
+    public Modulator() {
+
+    }
+
+    public static boolean isDesignatedTime(int hour) {
+        return hour % 6 == 0;
+    }
+
+    public int modulateTargetTemp(int lowestTemp, int targetTemp) {
+
+        if (lowestTemp <= 0) {
+            targetTemp += 2;
+        } else if (lowestTemp <= 5) {
+            targetTemp += 3;
+        } else if (lowestTemp <= 10) {
+            targetTemp += 4;
+        } else if (lowestTemp <= 15) {
+            targetTemp += 5;
+        } else if (lowestTemp <= 20) {
+            targetTemp += 6;
+        }
+
+        return targetTemp;
+    }
+
+
+}
